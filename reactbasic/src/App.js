5ccc69b47ba416c39d1503402aa581bb33377856
angdembe_sona <asonaxaviersm1@gmail.com>
2024-05-06 00:46:06 +0545

    added

---
+++ reactbasic/src/App.js
@@ -0,0 +1,17 @@
+
+const  App =()=> {
+  const vechile = ['BMW', 'tesla'];
+
+  const val = 1;
+  const val1 =1;
+ 
+
+      return (
+        <>
+      <div className="App" style= {{color:'red'}}>
+      <b>Hello world {(al+val1)},</b>
+        </div>
+        </>
+);
+};
+export default App;
