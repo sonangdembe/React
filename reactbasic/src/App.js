9cfca56a8c58914231a76c9373a0b07e6908d95c
angdembe_sona <asonaxaviersm1@gmail.com>
2024-05-06 09:35:16 +0545

    props

--- reactbasic/src/App.js
+++ reactbasic/src/App.js
@@ -1,17 +1,54 @@
 
-const  App =()=> {
-  const vechile = ['BMW', 'tesla'];
+// const  App =()=> {
+//   const vechile = ['BMW', 'tesla'];
 
-  const val = 1;
-  const val1 =1;
+//   const val = 1;
+//   const val1 =1;
  
 
-      return (
-        <>
-      <div className="App" style= {{color:'red'}}>
-      <b>Hello world {(al+val1)},</b>
-        </div>
-        </>
-);
+//       return (
+//         <>
+//       <div className="App" style= {{color:'red'}}>
+//       <b>Hello world {(al+val1)},</b>
+//         </div>
+//         </>
+// );
+// };
+// export default App;
+
+
+import React from 'react';
+import Contact  from "./components/Contact";
+const App= () =>{
+
+  const contacts = [
+    {
+      name:'john',
+      number:'456',
+       location: 'usa'
+    },
+    {
+      name:'john1',
+      number:'456',
+       location: 'nepal'
+    },
+    {
+      name:'john2',
+      number:'456',
+       location: 'ktm'
+    },
+  ];
+  return(
+ <>
+ <div className= "container"><h3>Contact List</h3>
+ <Contact data= {contacts[0]}/>
+ <Contact data= {contacts[1]} />
+ <Contact data= {contacts[2]}/>
+
+ </div>
+ </>
+  );
+
 };
+
 export default App;
---
+++ reactbasic/src/components/Contact.js
@@ -0,0 +1,16 @@
+const Contact =(props)=>{
+    return(
+<>
+<div className="contact">
+  {props.data.name}
+  <br/>
+  {props.data.number}
+  <br/>
+  {props.data.location}
+</div>
+<hr/>
+</>
+    );
+}
+
+export default Contact
